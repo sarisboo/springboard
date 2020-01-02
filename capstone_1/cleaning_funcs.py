a8e61e98f99147d22ecece7a3349f94af8d8bb1c
Sara <sarabouaz@gmail.com>
2020-01-02 17:35:47 +0100

    Added regex functions for cleaning and cleaned out irrelevant indices

--- capstone_1/cleaning_funcs.py
+++ capstone_1/cleaning_funcs.py
@@ -1,3 +1,33 @@
+import re
+
+
+r_clean_special_chars = re.compile(r'[^\w,.?!]|_')
+r_dedupe_spaces = re.compile(r'\s+')
+r_remove_newlines = re.compile(r'\n+')
+r_remove_empty_sentences = re.compile(r'([.!?])\W+[.!?]')
+r_sentence_boundary = re.compile(r'\s?[.!?]\s?')
+
+
+def clean_special_chars(text):
+    return r_clean_special_chars.sub(' ', text)
+
+
+def dedupe_spaces(text):
+    return r_dedupe_spaces.sub(' ', text)
+
+
+def remove_newlines(text):
+    return r_remove_newlines.sub('', text)
+
+
+def remove_empty_sentences(text):
+    return r_remove_empty_sentences.sub('\\1', text)
+
+
+def split_sentences(text):
+    return r_sentence_boundary.split(text)[:-1]
+
+
 def tup_list_maker(tup_list):
     """
     Takes a list of tuples with index 0 being the text_id and index 1 being a 
@@ -31,7 +61,7 @@ def is_sentence(sentence):
     return len(sentence.split(' ')) > 1
 
 
-def clean_n_char(sentence):
+def clean_newline(sentence):
     """
     Deletes the '\n' character at the begining of some sentences
     """
